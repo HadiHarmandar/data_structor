764cde7a28b8076b01854c9be3b38bcc8e3e9ca1
HadiHarmandar <hadiharmandar1@gmail.com>
2023-04-08 18:20:12 -0500

    LinkedList

---
+++ dsa-04-LinkedLists/src/main/java/SinglyLinkedListApp.java
@@ -0,0 +1,10 @@
+public class SinglyLinkedListApp {
+
+    public static void main(String[] args) {
+
+        MySinglyLinkedList myList = new MySinglyLinkedList();
+        System.out.println(myList.isEmpty());
+
+
+    }
+}
