01fd948f7ed645dc73a56aae6ae2afaf196605af
HadiHarmandar <hadiharmandar1@gmail.com>
2023-04-09 08:40:37 -0500

    Stacks

--- dsa-05-Stacks/src/main/java/SunsetViews.java
+++ dsa-05-Stacks/src/main/java/SunsetViews.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+
 public class SunsetViews {
     public static void main(String[] args) {
 
 
 
+    }
+    public ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
+        for (int i = 0; i < buildings.length; i++) {
+
+        }
+        return new ArrayList<Integer>();
     }
 }
